ebb760cdb1ace54a3ecf4d203d673032ed8c8811
L-Ocampo <134071058+L-Ocampo@users.noreply.github.com>
2023-10-14 21:10:11 +0800

    Update appcar6.py

--- appcar6.py
+++ appcar6.py
@@ -3,18 +3,25 @@ import tensorflow as tf
 from PIL import Image, ImageOps
 import numpy as np
 
-# Set the background color to black using markdown
+# Set the background color, text color, and font
 st.markdown(
     """
     <style>
-    .reportview-container {
-        background: black;
+    body {
+        background-color: black;
+        color: white;
+        font-family: Arial, sans-serif;
+    }
+    .stApp {
+        max-width: 800px;
+        padding: 10px;
     }
     </style>
     """,
     unsafe_allow_html=True
 )
 
+# Your Streamlit app code
 @st.cache_resource
 def load_model():
     model = tf.keras.models.load_model('cars_classifier.hdf5')
@@ -22,15 +29,13 @@ def load_model():
 
 model = load_model()
 
-st.write("""
-# Car Classifier
-""")
+st.title("Car Classifier")
 
 file = st.file_uploader("Choose a car photo from your computer", type=["jpg", "png"])
 
 def import_and_predict(image_data, model):
     size = (128, 128)
-    image = ImageOps.fit(image_data, size, Image.LANCZOS)  # Use Image.LANCZOS resampling filter
+    image = ImageOps.fit(image_data, size, Image.LANCZOS)
     img = np.asarray(image)
     img_reshape = img[np.newaxis, ...]
     prediction = model.predict(img_reshape)
@@ -43,5 +48,6 @@ else:
     st.image(image, use_column_width=True)
     prediction = import_and_predict(image, model)
     class_names = ['Audi', 'Hyundai Creta', 'Mahindra Scorpio', 'Rolls Royce', 'Swift', 'Tata Safari', 'Toyota Innova']
-    string = "OUTPUT: " + class_names[np.argmax(prediction)]
-    st.success(string)
+    result = class_names[np.argmax(prediction)]
+    st.subheader("Prediction:")
+    st.success(result)
