9e161f99c0955e68fbc988e7e2ba55c5320b1244
L-Ocampo <134071058+L-Ocampo@users.noreply.github.com>
2023-10-14 21:13:40 +0800

    Update appcar6.py

--- appcar6.py
+++ appcar6.py
@@ -3,56 +3,49 @@ import tensorflow as tf
 from PIL import Image, ImageOps
 import numpy as np
 
-# Set the background color, text color, and font
-st.markdown(
-    """
-    <style>
-    body {
-        background-color: black;
-        color: white;
-        font-family: Arial, sans-serif;
-        height: 100vh;
-        margin: 0;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .stApp {
-        max-width: 800px;
-        padding: 10px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-# Your Streamlit app code
-@st.cache_resource
+# Function to load the model
+@st.cache(allow_output_mutation=True)
 def load_model():
     model = tf.keras.models.load_model('cars_classifier.hdf5')
     return model
 
-model = load_model()
-
+# Page title and description
 st.title("Car Classifier")
+st.write("Upload a car photo, and I'll classify it into one of the following categories:")
 
+# Sidebar with explanation and credit
+st.sidebar.header("About")
+st.sidebar.markdown("This app uses a deep learning model to classify car images.")
+st.sidebar.markdown("Model credit: [Link to Model Source]")
+st.sidebar.markdown("App developed by [Your Name]")
+
+# Upload image
 file = st.file_uploader("Choose a car photo from your computer", type=["jpg", "png"])
 
+# Load the model
+model = load_model()
+
+# Function to make a prediction
 def import_and_predict(image_data, model):
     size = (128, 128)
-    image = ImageOps.fit(image_data, size, Image.LANCZOS)
+    image = ImageOps.fit(image_data, size, Image.LANCZOS)  # Use Image.LANCZOS resampling filter
     img = np.asarray(image)
     img_reshape = img[np.newaxis, ...]
     prediction = model.predict(img_reshape)
     return prediction
 
+# Display results
 if file is None:
-    st.text("Please upload an image file")
+    st.warning("Please upload an image file.")
 else:
     image = Image.open(file)
-    st.image(image, use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
     prediction = import_and_predict(image, model)
     class_names = ['Audi', 'Hyundai Creta', 'Mahindra Scorpio', 'Rolls Royce', 'Swift', 'Tata Safari', 'Toyota Innova']
-    result = class_names[np.argmax(prediction)]
+    class_index = np.argmax(prediction)
+    class_name = class_names[class_index]
+
     st.subheader("Prediction:")
-    st.success(result)
+    st.write(f"Class: {class_name}")
+    st.write(f"Confidence: {prediction[0][class_index]:.2%}")
