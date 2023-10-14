648d0054995e1824cca9e5ece54c9c1b35d0e541
L-Ocampo <134071058+L-Ocampo@users.noreply.github.com>
2023-10-14 21:06:13 +0800

    Update appcar6.py

--- appcar6.py
+++ appcar6.py
@@ -18,7 +18,7 @@ file = st.file_uploader("Choose a car photo from your computer", type=["jpg", "p
 
 def import_and_predict(image_data, model):
     size = (128, 128)
-    image = ImageOps.fit(image_data, size, 'antialias')
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)  # Use Image.ANTIALIAS directly
     img = np.asarray(image)
     img_reshape = img[np.newaxis, ...]
     prediction = model.predict(img_reshape)
