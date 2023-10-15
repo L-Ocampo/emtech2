f8c62eae6e2c9c973a7ff49141a0a42634b87e6f
L-Ocampo <qljocampo01@tip.edu.ph>
2023-10-15 23:38:12 +0800

    Test1

--- appcar5.py
+++ appcar5.py
@@ -13,12 +13,6 @@ def load_model():
 st.title("Car Classifier")
 st.write("Upload a car photo, and I'll classify it")
 
-# Sidebar with explanation and credit
-st.sidebar.header("About")
-st.sidebar.markdown("This app uses a deep learning model to classify car images.")
-st.sidebar.markdown("Model credit:")
-st.sidebar.markdown("App developed by Group# ")
-
 # Upload image
 file = st.file_uploader("Choose a car photo ", type=["jpg", "png"])
 
